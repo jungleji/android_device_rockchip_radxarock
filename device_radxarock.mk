579c9df549a00861a0d15db4c3c8f2d44e5d1d09
Ji Yonggang <jungleji@gmail.com>
2014-07-23 21:38:30 +0800

    Add ethernet support

--- device_radxarock.mk
+++ device_radxarock.mk
@@ -107,6 +107,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
 
 PRODUCT_COPY_FILES += \
     device/rockchip/radxarock/media_profiles_default.xml:system/etc/media_profiles_default.xml \
